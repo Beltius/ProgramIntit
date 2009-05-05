423e28f80cdcac2abcfb958a5522f6fffb63e118
Beltius <Beltius@qip.ru>
2009-05-05 15:10:35 +0300

    Edit window. Signed-off-by: Beltius <Beltius@qip.ru>

--- C++/Win/MyWinProgect/HelloFromMsgBox.cpp
+++ C++/Win/MyWinProgect/HelloFromMsgBox.cpp
@@ -3,6 +3,6 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
     LPSTR lpCmdLine, int nCmdShow)
 {
-	MessageBox(NULL, "Hello, Win32 world!", "Hello from Message Box", MB_OK);
+	MessageBox(NULL, "Hello!", "Hello from Message Box", MB_OK);
 	return 0;
 }
