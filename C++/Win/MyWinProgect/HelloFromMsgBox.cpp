2f9a87d67205e9e4b679a5db9879d2408dfd91a2
Beltius <Beltius@qip.ru>
2009-05-05 14:17:44 +0300

    first commit

---
+++ C++/Win/MyWinProgect/HelloFromMsgBox.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+    LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(NULL, "Hello, Win32 world!", "Hello from Message Box", MB_OK);
+	return 0;
+}
