f534618c9a0b327ecb9561db63d33b3afcc5b3f4
Beltius <Beltius@qip.ru>
2010-01-23 12:56:23 +0200

    добавлена д/з на 3 урок

--- C++/Shag/Shag 3/Shag 3/Shag 3.cpp
+++ C++/Shag/Shag 3/Shag 3/Shag 3.cpp
@@ -1,11 +1,12 @@
 #include "stdafx.h"
 #include <iostream>
+//#include <dos.h>
 using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int m, t, z, i, v, colsimvol, napravlenie;
-	int mas[10]={0,0,0,0,0,0,0,0,0,0};
-	cout << "Hello" << endl << "Dlia vibora zadania vvedite 1, 2, ili 3 : ";
+	int m, n, i, v, colsimvol, napravlenie, fact;
+Start:
+	cout << "Hello" << endl << "Dlia vibora zadania vvedite 1, 2, 3, ili 4 vihod : ";
 	cin >> v;
 		switch(v)
 		{
@@ -30,26 +31,54 @@ int _tmain(int argc, _TCHAR* argv[])
 					cout<< '\t' <<"*"<< endl;				
 				}
 			}
+			goto Start;
 			break;
 		case 2:
 			v = 0; 
 			cout << "Vvedite diapazon celih chisel: ";
-			for (i=0; i<=9; i++)
+			int mas[10];
+			for (i=0; i<9; i++)
 			{
 				cin >> m; 
-				mas[i-1]=m;
+				mas[i+1]=m;
 			}
-			for (i=0; i<=9; i++)
+			for (i=0; i<9; i++)
 				{
-					if (mas[i]%2==0)
+					if (mas[i]%2!=0)
 						{
 							v=v+mas[i];
 						}
 				}
 			cout << endl;
 			cout << v << endl;
+			goto Start;
+			break;
+		case 3:
+			fact = 1;
+			cout << "Vvedite chislo n: ";
+			cin >> n;
+			cout << endl;
+			if (n<0)
+				{
+					cout << "Choslo dolgo bit bolshe 0" << endl;
+				}
+			else if (n==0)
+				{
+					cout << "Factorial chisla raven : 1" << endl;
+				}
+			else {
+				for (i=1; i<=n; i++)
+					{
+						fact=fact*i;
+					}
+				cout << "Factorial chisla raven :" << fact << endl;
+			}
+			goto Start;
+			break;
+		case 4:
+			break;
 		}
-
-	return 0; 
+//_sleep(5000);
+return 0; 
 }
 
