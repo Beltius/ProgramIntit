182808560619d067a5f058a3219b48629a118ddf
Beltius <Beltius@qip.ru>
2010-01-24 08:48:15 +0200

    Доработан 5 урок и все тексты переведены в Unicode.

--- C++/Shag/Shag 5/Shag 5/Shag 5.cpp
+++ C++/Shag/Shag 5/Shag 5/Shag 5.cpp
@@ -1,27 +1,51 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <locale.h>
 using namespace std;
 int main()
 {
-	int a, b, i, t, j, zadan;
+	system ("color 0a");
+	int a, b, t, j, zadan, i;
+	setlocale (LC_ALL, "rus");
+	const int week=7, days=31;
 Start:
-	cout << "Vvedite nomer zadania 1, 2, 3 ili 4 vihod : ";
+	cout << "1. Создать программу, которая выводит на экран простые числа в диапазоне от 2 до 1000." <<endl;
+	cout << "(Число называется простым, если оно делится только на 1 и на само себя без остатка; " <<endl;
+	cout << "причем числа 1 и 2 простыми не считаются). " <<endl;
+	cout << "2. Написать программу, которая выводит на экран - следующую фигуру:" <<endl;
+	cout << "*********************" <<endl;
+	cout << "*                   *" <<endl;
+	cout << "*                   *" <<endl;
+	cout << "*                   *" <<endl;
+	cout << "*                   *" <<endl;
+	cout << "*                   *" <<endl;
+	cout << "*                   *" <<endl;
+	cout << "*********************" <<endl;
+	cout << "ширина и высота фигуры задаются пользователем с клавиатуры." <<endl;
+	cout << "3. При помощи цикла показать на экран календарь текущего месяца." <<endl;
+	cout << "Введите номер задания : "<<endl;
 	cin >> zadan;
 	switch(zadan)
 	{
 	case 1:
-		for(int i=1;i<1001;i++)
+		for(int i=1;i<101;i++)
 		{
-			for(int j=2;j<1001;j++)
+			for(int j=2;j<101;j++)
 			{
-				if (i%j==0 && i!=j)
+				/*if (i%j==0 && i!=j)
 					{
-					cout << "neprostoe      " << i << "     " << j << endl;
+					cout << "непростое      " << i << "     " << j <<endl;
 					break;
-					}
-				else if (i%j==0)
+					}*/
+				 if (i%j==0)
 					{
-					cout << "prostoe   " << i << "   "<< j << endl;
+						if (i!=j)
+						{
+							break;
+						}
+					cout << "простое   " << i/* << "   "<< j */<< endl;
 					break;
 					}
 			}
@@ -29,10 +53,10 @@ Start:
 		goto Start;
 		break;
 	case 2:
-		cout << "Vvedite shirinu : ";
+		cout << "Введите ширину фигуры : ";
 		cin >> a;
 		cout << endl;
-		cout << "Vvedite visotu : ";
+		cout << "Введите высоту фигуры : ";
 		cin >> b;
 		cout << endl;
 		for (i=1; i<=a; i++)
@@ -58,16 +82,28 @@ Start:
 		goto Start;
 		break;
 	case 3:
-	 for(int j=1;j<31;j++)
-        {
-            cout<<j<<"\t";
-
-            if (j%7 == 0)
-            {
-                cout<<"\n";
-            } 
-        }
-	 cout << endl;
+	cout<<"Пн"<<setw(5)<<"Вт"<<setw(5)<<"Ср"<<setw(5)<<"Чт"<<setw(5)<<"Пт"<<setw(5)<<"Сб"<<setw(5)<<"Вс"<<setw(5)<<endl;
+	for(int i=0; i<=week; ++i)
+	{
+		cout<<"_____";
+	}
+	for(int i=0; i<5; ++i)
+	{
+		cout<<setw(5);
+	}
+	for(int i=1; i<=3; ++i)
+	{
+		cout<<i<<setw(5);
+	}
+	cout<<endl;
+	for (int i=4; i<days; i+7)
+	{
+		for(int j=1; j<=week; ++j, ++i)
+		{
+			cout<<i<<setw(5);
+		}
+		cout<<endl;
+	}
 		goto Start;
 		break;
 	case 4:
